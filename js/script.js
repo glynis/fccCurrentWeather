c268466fcfaa331593fa7b8fe1099d0e9dcbf77b
glynis <contact@glyn.is>
2017-11-07 17:09:24 -0500

    final update
    meets all fcc project requirements

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-/* glyn.is.950: 29.08.2017 - 05.09.2017 */
+/* glyn.is.950: 29.08.2017 - 07.11.2017 */
 $(document).ready(function(){
     // GET LOCATION
     $.get("http://freegeoip.net/json/", function (response) {
         $("#city").html(response.city + ", " + response.region_name);
         var searchQuery = (response.region_code + "/" + response.city + ".json");
-        // GET WEATHER FOR THIS LOCATION
+    // GET WEATHER FOR THIS LOCATION
         $.ajax({
    		    url : "http://api.wunderground.com/api/9273aaedb10bc501/conditions/q/" + searchQuery,
 		    dataType : "jsonp",
@@ -12,14 +12,16 @@ $(document).ready(function(){
 		        var conditions = data.current_observation.weather;
 		        var icon = data.current_observation.icon;
                 var tempC = data.current_observation.temp_c;
+                var tempF = data.current_observation.temp_f;
 
                 $('#conditions').html(conditions);
                 $('#temperature').html(tempC);
+                $('#scale').html(" &deg;C");
 
-                // MATCH CONDITIONS
-                if (icon == "clear") {
+    // MATCH CONDITIONS
+              if (icon == "clear") {
                     $('#icond').html('<i class="fa fa-sun-o"></i>');
-                } else if (icon == "partlycloudy") {
+                } else if (icon == "partlycloudy" || icon == "cloudy") {
                     $('#icond').html('<i class="fa fa-cloud"></i>');
                 } else if (icon == "rain") {
                     $('#icond').html('<i class="fa fa-tint"></i>');
@@ -28,10 +30,10 @@ $(document).ready(function(){
                 } else {
                 	console.log("Unrecognized icon: \"" + icon + "\"")
                     $('#icond').html('<i class="fa fa-question"></i>');
-                }
+              } // endif
 
-				// MATCH TEMPERATURE
-                if (tempC <= 0) {
+		// MATCH TEMPERATURE
+              if (tempC <= 0) {
                     $('#itemp').html('<i class="fa fa-thermometer-empty"></i>');
                 } else if (tempC >= 1 && tempC <= 10) {
                     $('#itemp').html('<i class="fa fa-thermometer-quarter"></i>');
@@ -43,9 +45,19 @@ $(document).ready(function(){
                     $('#itemp').html('<i class="fa fa-thermometer-full"></i>');
                 } else {
                     $('#itemp').html('<i class="fa fa-question"></i>');
-                }
-
-            }
-        });
-    });
-});
+              } // endif
+    // TEMPERATURE BUTTONS
+          // on button click, buttC - change temperature to tempC, scale to C
+                $('#buttC').click(function() {
+                  $('#temperature').html(tempC);
+                  $('#scale').html(" &deg;C");
+                });
+          // on button click, buttF - change temperature to tempF, scale to F
+                $('#buttF').click(function() {
+                  $('#temperature').html(tempF);
+                  $('#scale').html(" &deg;F");
+                });
+            } // (data)
+        }); // (ajax)
+    }); // (get)
+}); // (ready)
